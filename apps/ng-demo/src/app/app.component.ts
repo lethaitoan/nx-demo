598c164606c624e6bb6f262e92fe069ca20d76f1
tvtle <tvtle@codix.eu>
2021-10-26 13:15:19 +0700

    initial commit

---
+++ apps/ng-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'ng-demo';
+}
